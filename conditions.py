b309e614cfd5e9154ca350fa3ba87e15eb35aab6
Gautam Diwan <gautam.diwan@hcode.tech>
2024-03-11 11:31:51 +0530

    Add conditions.py

---
+++ conditions.py
@@ -0,0 +1,17 @@
+age = 18
+CanDrive = True
+if age >= 18:
+    print("adult")
+else:
+    print("kid")
+
+if age>=16: print("Mature")
+
+if age >= 18 and CanDrive:
+    print("Reliable")
+elif CanDrive:
+    print("Illegal driving")
+
+if age >=25:
+    pass
+

1c962dd9d4c7ece03262aa67e6d0307143dfe832
Gautam Diwan <gautam.diwan@hcode.tech>
2024-03-11 11:15:52 +0530

    Complete string operations

---
+++ strings.py
@@ -0,0 +1,29 @@
+name = "Gautam"
+age = 22
+print("%s is %d years old" % (name, age))
+
+languages = ['Hindi', 'Punjabi', 'English']
+
+print("Known Languages are %s" % languages)
+print(', '.join(languages))
+
+pi = 3.14159
+print("value of pi at 2 digits is: %.2f" % pi)
+print("age in hex is: %x" % age)
+
+MultilineString = '''This is
+multiline 
+string
+'''
+
+print(MultilineString)
+print("This" in MultilineString)
+print(MultilineString.index("multi"))
+print(MultilineString.find("multi"))
+
+pi = "3.14"
+print(pi.isdigit())
+print(f"{pi}")
+
+number = 123
+print("{number}".format(number = number))
